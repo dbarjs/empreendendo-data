4cc9df659478db84fd1246156ef9a595c29ace56
dudusbarros <joseeduardobarros@gmail.com>
2017-02-03 09:42:59 -0200

    Adicionado arquivos baseados no arquivo

---
+++ public/dist/js/empreendendo-base.min.js
@@ -0,0 +1,5 @@
+/*!
+ * Elbit Empreendendo Data Project v0.0.0 (http://elbit.com.br/)
+ * Copyright 2013-2016 Elbit Developers
+ * Licensed under MIT (https://github.com/elbitdigital/base/blob/master/LICENSE)
+*/
